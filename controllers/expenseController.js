47a0c1db234eb3c1859beeeb0082f9550efdc7dd
Vincent <Vincentnguyen030@gmail.com>
2024-05-24 14:37:32 -0500

    Create home page

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -5,72 +5,70 @@ const asyncHandler = require("express-async-handler");
 
 // Function to sum up all prices in the expenses schema
 const sumAllPrices = async () => {
-    try {
-      // Perform an aggregation on the Expense collection
-      const result = await Expense.aggregate([
-        {
-          $group: {
-            _id: null, // Use null to group all documents together
-            totalPrice: { $sum: "$price" } // Sum the values of the 'price' field
-          }
+  try {
+    // Perform an aggregation on the Expense collection
+    const result = await Expense.aggregate([
+      {
+        $group: {
+          _id: null, // Use null to group all documents together
+          totalPrice: { $sum: "$price" } // Sum the values of the 'price' field
         }
-      ]);
-  
-      if (result.length > 0) {
-        // Log the total price of all expenses
-        return result[0].totalPrice; // Return the total price
-      } else {
-        return 0;
       }
-    } catch (error) {
-      throw error;
+    ]);
+
+    if (result.length > 0) {
+      // Log the total price of all expenses
+      return result[0].totalPrice; // Return the total price
+    } else {
+      return 0;
     }
+  } catch (error) {
+    throw error;
+  }
 };
 
 const getItemsByDateRange = async (startDate, endDate) => {
-    try {
-      const start = new Date(startDate);
-      const end = new Date(endDate);
-  
-      const items = await Expense.find({
-        date_bought: {
-          $gte: start,
-          $lte: end
-        }
-      });
-  
-      return items;
-    } catch (error) {
-      console.error('Error fetching items by date range:', error);
-      throw error;
-    }
-  };  
+  try {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    const items = await Expense.find({
+      date_bought: {
+        $gte: start,
+        $lte: end
+      }
+    });
+
+    return items;
+  } catch (error) {
+    throw error;
+  }
+};  
 
 // home page
 exports.index = asyncHandler(async (req, res, next) => {
-    const [
-        numExpenses,
-        numSpendingCategories,
-        numStores,
-        totalSpent
-    ] = await Promise.all([
-        Expense.countDocuments({}).exec(),
-        SpendingCategory.countDocuments({}).exec(),
-        Store.countDocuments({}).exec(),
-        sumAllPrices()
-    ]);
-    res.render("layout",{
-        title:"Home",
-        expense_count:numExpenses,
-        spending_category_count:numSpendingCategories,
-        store_count:numStores,
-        total_spent:totalSpent
-    })
+  const [
+      numExpenses,
+      numSpendingCategories,
+      numStores,
+      totalSpent
+  ] = await Promise.all([
+      Expense.countDocuments({}).exec(),
+      SpendingCategory.countDocuments({}).exec(),
+      Store.countDocuments({}).exec(),
+      sumAllPrices()
+  ]);
+  res.render("layout",{
+      title:"Home",
+      expense_count:numExpenses,
+      spending_category_count:numSpendingCategories,
+      store_count:numStores,
+      total_spent:totalSpent
+  })
 });
 
 // Display list of all expenses.
 exports.expense_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: expense list");
 });
   
 // Display detail page for a specific expense.
